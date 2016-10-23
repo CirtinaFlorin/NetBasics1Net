e65f9417e50d5c0f7e41a22bb74a6c958456c5ca
CirtinaFlorin <florin.cirtina@outlook.com>
2016-10-23 20:40:31 +0300

    First commit on CourseNEt

---
+++ ClassesTutorial/ClassesTutorial/Mercedes.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassesTutorial
+{
+    internal class Mercedes: AbstractCar
+    {
+        internal string Description;
+        internal int Price;
+        private string _description;
+        private int _price;
+        public Mercedes() {
+            Description = "E Class";
+            Price = 67000;
+        }
+    }
+}
---
+++ ClassesTutorial/ClassesTutorial/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassesTutorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Mercedes mercedes = new Mercedes();
+            EngineStruct engine = new EngineStruct();
+            ICar car = new Mercedes();
+            AbstractCar car2 = new Mercedes();
+            Console.WriteLine("This is my first project!");
+            Console.WriteLine("Mercedes is "+ mercedes.Description);
+            Console.WriteLine("Mercedes costs " + mercedes.Price);
+            Console.ReadKey();
+        }
+    }
+}

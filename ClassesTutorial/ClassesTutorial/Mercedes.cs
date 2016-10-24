d669c4f542623c8d38bcdf2db4fae4127f7e21e6
CirtinaFlorin <florin.cirtina@outlook.com>
2016-10-24 19:22:37 +0300

    Laborator2

--- ClassesTutorial/ClassesTutorial/Mercedes.cs
+++ ClassesTutorial/ClassesTutorial/Mercedes.cs
@@ -16,5 +16,24 @@ namespace ClassesTutorial
             Description = "E Class";
             Price = 67000;
         }
+
+        private int counter;
+
+        public void PrintMercedesLogo() {
+            counter++;
+            if (counter == 3) return;
+            Console.WriteLine("This is from Mercedes");
+            PrintMercedesLogo();
+        }
+
+        public void PrintNewMercedes(int c) {
+            if (c == 3) return;
+            Console.WriteLine("Mercedes No = " + c);
+        }
+        public static int staticcounter;
+
+        public static void StartEngine() {
+            Console.WriteLine("Engine Started");
+        }
     }
 }
--- ClassesTutorial/ClassesTutorial/Program.cs
+++ ClassesTutorial/ClassesTutorial/Program.cs
@@ -10,14 +10,47 @@ namespace ClassesTutorial
     {
         static void Main(string[] args)
         {
-            Mercedes mercedes = new Mercedes();
-            EngineStruct engine = new EngineStruct();
-            ICar car = new Mercedes();
-            AbstractCar car2 = new Mercedes();
-            Console.WriteLine("This is my first project!");
-            Console.WriteLine("Mercedes is "+ mercedes.Description);
-            Console.WriteLine("Mercedes costs " + mercedes.Price);
+            //Mercedes mercedes = new Mercedes();
+            //EngineStruct engine = new EngineStruct();
+            //ICar car = new Mercedes();
+            //AbstractCar car2 = new Mercedes();
+            //PrintLogo();
+            //PrintLogo(7);
+            //Console.WriteLine("This is my first project!");
+            //Console.WriteLine("Mercedes is "+ mercedes.Description);
+            //Console.WriteLine("Mercedes costs " + mercedes.Price);
+
+            Mercedes m1 = new Mercedes();
+            //m.PrintMercedesLogo();
+            m1.PrintNewMercedes(Mercedes.staticcounter++);
+            Mercedes m2 = new Mercedes();
+            Mercedes m3 = new Mercedes();
+            Mercedes m4 = new Mercedes();
+            m2.PrintNewMercedes(Mercedes.staticcounter++);
+            m3.PrintNewMercedes(Mercedes.staticcounter++);
+            m4.PrintNewMercedes(Mercedes.staticcounter++);
+
+            Mercedes.StartEngine();
             Console.ReadKey();
+
+            
+        }
+
+        static int counter;
+
+        static void PrintLogo() {
+            Console.WriteLine("I am learning C#");
+            Console.WriteLine("C#is great");
+            PrintLogo2();
+        }
+        static void PrintLogo(int i) {
+            Console.WriteLine("I am another printlogo");
+        }
+        static void PrintLogo2() {
+            counter++;
+            if (counter == 3) return;
+            Console.WriteLine("PrintLogo2");
+            PrintLogo();
         }
     }
 }
